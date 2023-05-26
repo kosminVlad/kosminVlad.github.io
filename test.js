19ea8ffc62ce89d4ba9be1b832e5c85cfdc38bdd
KosminVlad <ksagwey.vlad@mail.ru>
2023-05-26 15:23:50 +0700

    123

--- test.js
+++ test.js
@@ -1,12 +1,14 @@
 let correct = [1, 0]
-let input = [0,0]
+let input = [0, 0]
 let sum = 0
+const res = document.getElementById('res')
 
 function tap(x) {
-    input = [0,0]
-    if(input[x] == 1) {
+    input = [0, 0]
+    if (input[x] == 1) {
         input.splice(x, 1, 0)
         console.log(input)
+        sum += 1
     }
     else {
         input.splice(x, 1, 1)
@@ -14,11 +16,6 @@ function tap(x) {
     }
 }
 function n() {
-        sum = 0
-        for(i = 0; i < correct.length; i++){
-            if(input[i] == correct[i]) {
-                sum += 1
-            }
-        }
-        console.log(sum);
-    }
+    res.innerHTML = `Ваш результат: ${sum}`
+}
+
