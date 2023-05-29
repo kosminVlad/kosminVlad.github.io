2b515f3c3622652dc380939d0c39316a7616fd32
KosminVlad <ksagwey.vlad@mail.ru>
2023-05-29 16:43:09 +0700

    21312

--- test.js
+++ test.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-let correct1 = [1,]
-let correct2 = [2,]
-let input = [0, 0]
-let sum = 0
-const res = document.getElementById('res')
-
-function tap(x) {
-    input = [0, 0]
-    if (input[x] == 1) {
-        input.splice(x, 1, 0)
-        console.log(input)
-    }
-    else {
-        input.splice(x, 1, 1)
-        console.log(input)
-    }
-}
-function n() {
-    sum = 0
-    for( i = 0; i < correct.length; i++) {
-        if(input[i] == correct[i]){
-            sum += 1
-        }
-    }
-    res.innerHTML = `Ваш результат: ${sum}`
-}
-
-=======
 let correct = [1, 3, 2, 1] 
 let input = [] 
 let sum = 0 
@@ -53,4 +24,3 @@ function n() {
     } 
     // res.innerHTML = `Ваш результат: ${sum}`; 
     } 
->>>>>>> d43adda228a11c685c68971b11adc3fc43a196a1
